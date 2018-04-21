5b4e02b9250f7f98411b9ce1529cb6a9de8ba947
Thomas Feron <thomas.feron@redspline.com>
2018-04-21 17:21:43 +0200

    First commit (moved from Optinomic/apps)

---
+++ _properties.js
@@ -0,0 +1,54 @@
+properties: {
+
+
+    _pdf_full_actinfo: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    },
+    _pdf_full_tmt: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    },
+    _pdf_full_bscl: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    },
+    _pdf_full_asrs: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    },
+    _pdf_full_aase: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    },
+    _pdf_full_bdi: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    },
+    _pdf_full_whoqol: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    },
+    _pdf_full_isk: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    },
+    _pdf_full_sci: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    },
+    _pdf_app_info: {
+        type: Object,
+        value: null,
+        observer: '_buildPDF'
+    }
+},
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Übersicht | Fallkonferenz     
+
+### Zusammenfassung
+Eine schnelle Übersicht über die wichtigsten Ergebnisse der während der Behandlung erhobenen Fragebögen.
